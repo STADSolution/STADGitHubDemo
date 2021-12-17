7d3437ea3d7b072663102b6ec9ba2a884daa9516
STAD solution <training@stadsolution.com>
2021-12-17 17:24:08 +0530

    second commit

--- src/main/java/com/pages/LoginPage.java
+++ src/main/java/com/pages/LoginPage.java
@@ -3,8 +3,8 @@ package com.pages;
 public class LoginPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	System.out.println("Hello in login Page");
+	System.out.println("This is new line");
 	}
 
 }
